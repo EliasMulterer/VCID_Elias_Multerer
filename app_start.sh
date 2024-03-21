8bba562a7b2027c4fc89385ca85434a10a4e564a
Elias Multerer <elias.multerer@gmail.com>
2024-03-21 19:05:09 +0100

    Update app_start.sh

--- app_start.sh
+++ app_start.sh
@@ -2,7 +2,7 @@
 
 python3 -m venv venv
 source venv/bin/activate
-pip install -r requirements.txt
+pip install -r /home/elias_multerer/VCID_Elias_Multerer/requirements.txt
 
 #Starten der Applikation via gunicorn über Port 8080
 gunicorn -b :8080 app:app &
